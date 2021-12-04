13f4ca4bcc75576cff88ec75b3145d17398c4975
Maher Wardak <maher.wardak@gmail.com>
2021-12-04 10:49:50 -0500

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="home-container">hello</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+// require("dotenv").config();
+const app = express();
+// const axios = require("axios");
+
+app.use("/", express.static(path.join(__dirname, "client/build")));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server listening on port ${port}`));
