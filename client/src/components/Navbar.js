4287339c414a8fae6084941aa959c08157030f43
Maher Wardak <maher.wardak@gmail.com>
2021-12-04 23:32:50 -0500

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,23 +2,44 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
-// import Grid from "@material-ui/core";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import CssBaseline from "@mui/material/CssBaseline";
+import { NavLink } from "react-router-dom";
 
-// import { Link as RouterLink } from "react-router-dom";
+// import Grid from "@material-ui/core";
 
 const Navbar = () => {
   return (
     <AppBar position="static">
-      <Toolbar style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Typography>
-          <Link color="inherit">Home</Link>
+      <CssBaseline />
+      <Toolbar>
+        <Box
+          mr={45}
+          sx={{
+            flexGrow: 1,
+            alignItems: "right",
+          }}
+        >
+          <TwitterIcon />
+        </Box>
+        <Typography variant="h5">
+          <Box m={1}>
+            <Link color="inherit">Home</Link>
+          </Box>
         </Typography>
-        <Typography>
-          <Link color="inherit">Search</Link>
+
+        <Typography variant="h5">
+          <Box m={1}>
+            <Link color="inherit">Search</Link>
+          </Box>
         </Typography>
-        <Typography>
-          <Link color="inherit">Random</Link>
+
+        <Typography variant="h5">
+          <Box m={1}>
+            <Link color="inherit">Random</Link>
+          </Box>
         </Typography>
       </Toolbar>
     </AppBar>
