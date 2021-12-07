c2414aa194a2f8c797d5ceff6b83e0763d63e61b
Maher Wardak <maherwardak@Mahers-MacBook-Pro.local>
2021-12-06 23:27:11 -0800

    search component WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 // import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 
 import Home from "./components/Home";
@@ -18,7 +17,9 @@ const App = () => {
     //   </Switch>
     // </Router>
     <div>
-      <Navbar />
+      {/* <Navbar />
+      <Home /> */}
+      <Search />
     </div>
   );
 };
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,37 @@
 import React from "react";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import CssBaseline from "@mui/material/CssBaseline";
+
 const Search = () => {
-  return <div></div>;
+  return (
+    <form>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <CssBaseline />
+        <Box m={2}>
+          <TextField
+            id="outlined-basic"
+            label="Search Twitter..."
+            variant="outlined"
+            size="small"
+            fullWidth
+          />
+        </Box>
+
+        <Button variant="contained" color="primary" size="small">
+          Search
+        </Button>
+      </Box>
+    </form>
+  );
 };
 
 export default Search;
