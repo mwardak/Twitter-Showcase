68c8f0bffd5591290b394de75b130a0ccc422fee
Maher Wardak <maherwardak@Mahers-MacBook-Pro.local>
2022-01-10 22:44:22 -0500

    tweetcard almost complete

--- client/src/components/TweetCard.js
+++ client/src/components/TweetCard.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import "./TweetCard.css";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import RepeatIcon from "@mui/icons-material/Repeat";
@@ -7,45 +6,59 @@ import { Avatar } from "@material-ui/core";
 import avatar from "./avatar.png";
 
 const TweetCard = ({ tweets }) => {
-  const tweetCards = tweets?.data?.map((tweet) => {
-    // merge data and includes into an array
-    const tweetData = Object.assign({}, tweet.data, tweet.includes);
+  const { data } = tweets;
+  const { users } = tweets.includes;
 
-    console.log(tweetData);
+  if (data) {
+    function mergeObjects(arr1, arr2) {
+      return arr1.map((item, index) => {
+        return {
+          ...item,
+          ...arr2[index],
+        };
+      });
+    }
+    const merged = mergeObjects(data, users);
+    const tweetCards = merged.map((tweet) => {
+      return (
+        <div className="tweet" key={tweet.id}>
+          <div className="tweet__avatar">
+            <Avatar
+              src={tweet.profile_image ? tweet.profile_image_url : avatar}
+              alt="avatar"
+            />
+          </div>
+          <div className="tweet__body">
+            <div className="tweet__header">
+              <div className="tweet__headerText">
+                <h3>{tweet.user}</h3>
 
-    return (
-      <div className="tweet" key={tweet.id}>
-        <div className="tweet__avatar">
-          <Avatar src={avatar} alt="avatar" />
+                <p className="tweet__twitterHandle">@{tweet.username}</p>
+              </div>
+              <div className="tweet__headerDescription"></div>
+            </div>
+            <div className="tweet__content">
+              <p>{tweet.text}</p>
+            </div>
+            <div className="tweet__footer">
+              <div className="tweet__footerIcons">
+                <p>{tweet.public_metrics.like_count}</p>
+                <ThumbUpIcon />
+              </div>
+              <div className="tweet__footerIcons">
+                <p>{tweet.public_metrics.retweet_count}</p>
+                <RepeatIcon />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="tweet__body">
-          <div className="tweet__header">
-            <div className="tweet__headerText">
-              <h3>{tweet.user}</h3>
+      );
+    });
 
-              <p className="tweet__twitterHandle">@{}</p>
-            </div>
-            <div className="tweet__headerDescription"></div>
-          </div>
-          <div className="tweet__content">
-            <p>{tweet.text}</p>
-          </div>
-          <div className="tweet__footer">
-            <div className="tweet__footerIcons">
-              <p>{tweet.public_metrics.like_count}</p>
-              <ThumbUpIcon />
-            </div>
-            <div className="tweet__footerIcons">
-              <p>{tweet.public_metrics.retweet_count}</p>
-              <RepeatIcon />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  });
-
-  return <div>{tweetCards}</div>;
+    return <div>{tweetCards}</div>;
+  } else {
+    return null;
+  }
 };
 
 export default TweetCard;
